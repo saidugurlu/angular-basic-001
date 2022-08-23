78847e9d264c3b726fa668e9272a9965141e9dfb
saidugurlu <ugurlumsaid@gmx.de>
2022-08-23 13:18:10 +0200

    bootstrap in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app',
+   templateUrl: './app.component.html',
+/*   template: `
+
+  <h1>test angular basic</h1>
+  <h2>{{ title }}</h2>
+  <h2>{{ getTitle() }}</h2>
+  <p>{{ todoItems.description }}</p>
+  <p>{{ todoItems.done }}</p>
+
+    `, */
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Todo App'; // Burada tanimladigimiz title componentini app.component.html dosyasinda gosteriyoruz.
+  /*   private title = "Todo App"; */
+
+  getTitle() {
+    return this.title;
+  }
+
+  todoItems = {
+    description: 'breakfast',
+    done: false,
+  };
 }
