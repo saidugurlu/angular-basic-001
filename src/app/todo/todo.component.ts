bc85fe972bb9dbd583fbe2c90ed663bb836c4767
saidugurlu <ugurlumsaid@gmx.de>
2022-08-26 11:52:40 +0200

    model for todo in

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent implements OnInit {
+export class TodoComponent {
+  constructor() {}
+  private name: string = 'Said';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getName() {
+    return this.name;
   }
-
+  items: any[] = [
+    { description: 'Einkaufen', action: 'no' },
+    { description: 'Kochen', action: 'yes' },
+    { description: 'Sport ', action: 'no ' },
+  ];
 }
